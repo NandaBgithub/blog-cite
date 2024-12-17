1c1446adae097b3ef0f94fbb352fe37027387518
NandaBgithub <nandaputub@gmail.com>
2024-12-18 09:47:23 +1100

    adding build dist to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/blog-cite/",
+  build: {
+    outDir: 'dist', // Default output folder for Vite
+  },
 })
