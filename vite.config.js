f75ba280ebf19922f0763cdb77c117fd3eea8794
NandaBgithub <nandaputub@gmail.com>
2024-12-18 10:07:50 +1100

    editing base in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blog-cite/src",
-  build: {
-    outDir: 'dist', // Default output folder for Vite
-  },
+  base: "/blog-cite/"
 })
